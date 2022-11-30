7dd053c26b0b612500e7d285a1fa3e36e211a909
zhoujason <7582985009@qq.com>
2022-11-30 22:08:07 +0800

    Initial commit

---
+++ 编程6-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    float number, max;
+    printf("Enter a number: ");
+    scanf("%f", &number);
+    max = number;
+    while (number > 0){
+        printf("Enter a number: ");
+        scanf("%f", &number);
+        if (number > max) max = number;
+    }
+    printf("The largest number entered was %g", max);
+
+    return 0;
+}
